1a99527208f9ad6e5be1520017031751f493a729
theWebCookie <jabwojtek@wp.pl>
2023-08-06 17:33:48 +0200

    add _document file

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,8 @@ export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        <title>Next Events</title>
+        <meta name='description' content='NextJS Events' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
       <Component {...pageProps} />
