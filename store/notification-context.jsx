619ed29b967b2e4fa164641339d446cf14ca75dc
theWebCookie <jabwojtek@wp.pl>
2023-08-10 15:15:24 +0200

    automatically remove notifications

--- store/notification-context.jsx
+++ store/notification-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const NotificationContext = createContext({
   notification: null,
@@ -10,9 +10,21 @@ export default NotificationContext;
 
 export const NotificationContextProvider = (props) => {
   const [activeNotification, setActiveNotification] = useState();
+  useEffect(() => {
+    if (activeNotification && (activeNotification.status === 'success' || activeNotification.status === 'error')) {
+      const timer = setTimeout(() => {
+        setActiveNotification(null);
+      }, 3000);
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
+
   const showNotificationHandler = (notificationData) => {
     setActiveNotification(notificationData);
   };
+
   const hideNotificationHandler = () => {
     setActiveNotification(null);
   };
