7dc53d6d6c6f2b40a1105ce5df59ad9f55fa1b43
theWebCookie <jabwojtek@wp.pl>
2023-08-10 15:04:51 +0200

    show notifications based on context

--- components/input/newsletter-registration.jsx
+++ components/input/newsletter-registration.jsx
@@ -1,18 +1,22 @@
-import { useRef } from 'react';
+import { useContext, useRef } from 'react';
 import classes from './newsletter-registration.module.css';
+import NotificationContext from '@/store/notification-context';
 
 const NewsletterRegistration = () => {
+  const notificationCtx = useContext(NotificationContext);
+
   const emailInputRef = useRef();
   const registrationHandler = (event) => {
     event.preventDefault();
-    if (!emailInputRef.current.value) {
-      console.log('No value');
-      return;
-    }
-
     const reqBody = {
       email: emailInputRef.current.value,
     };
+
+    notificationCtx.showNotification({
+      title: 'Signing up...',
+      message: 'Registering for newsletter.',
+      status: 'pending',
+    });
 
     fetch('/api/newsletter/', {
       method: 'POST',
@@ -21,8 +25,28 @@ const NewsletterRegistration = () => {
         'Content-Type': 'application/json',
       },
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return response.json().then((data) => {
+          throw new Error(data.message || 'Something went wrong!');
+        });
+      })
+      .then(() => {
+        notificationCtx.showNotification({
+          title: 'Success',
+          message: 'Successfully registered for newsletter!',
+          status: 'success',
+        });
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: 'Error!',
+          message: error.message || 'Something went wrong!',
+          status: 'error',
+        });
+      });
   };
 
   return (
